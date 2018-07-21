1822322b1b158cc4adc7fa3367b4ec5948ff9828
Denis Beliakov <workbel14@gmail.com>
2018-07-21 15:26:53 +0300

    added: apache pack + initial controllers

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends Controller
+{
+    /**
+     * @Route("/")
+     *
+     * @return JsonResponse
+     */
+    public function index()
+    {
+        return new Response(
+            '<html><body>Lucky number: 11</body></html>'
+        );
+    }
+}
